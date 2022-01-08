de026320444ede7279f3d736819418bef86ec423
Rogerio Cesar da Nobrega <rogeriocnobrega@gmail.com>
2022-01-07 22:57:19 -0300

    migrations com Flyway

---
+++ rest-with-springboot-udemy/src/main/resources/db/migration/V2__Populate_Table_Person2.sql
@@ -0,0 +1,4 @@
+INSERT INTO rest_with_springboot_udemy.persons (address,first_name,gender,last_name) VALUES
+	 ('Rua sem nome','Gariela 2222','Female','LastName'),
+	 ('Rua sem nome','Gariela Prioli','Female','LastName'),
+	 ('Rua sem nome','Gariela Mosov2','Female','LastName');
